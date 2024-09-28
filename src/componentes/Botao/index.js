7551e46a441df9b0e5f4c16a8d45215af5530284
Nathanbsy <nathanbs1227@gmail.com>
2024-09-28 13:09:49 -0300

    Criando botão e lista suspensa

---
+++ src/componentes/Botao/index.js
@@ -0,0 +1,11 @@
+import "./Botao.css"
+
+
+
+const Botao = (props) => {
+    return (
+        <button className="btn">{props.children}</button>
+    )
+}
+
+export default Botao
--- src/componentes/Formulario/index.js
+++ src/componentes/Formulario/index.js
@@ -1,14 +1,33 @@
 import "./Formulario.css"
 import CampoTexto from '../../componentes/CampoTexto';
+import ListaSuspensa from "../ListaSuspensa";
+import Botao from "../Botao";
 
 const Formulario = () => {
+    const times = [
+        "Programação",
+        "Front-End",
+        "Data Science",
+        "DevOps",
+        "UX e Design",
+        "Mobile",
+        "Inovação e Gestão"
+    ]
+
+    const aoSalvar = (evento) => {
+        evento.preventDefault();
+        console.log("Formulario enviado");
+    }
+
     return (
         <section className="formulario">
-            <form>
+            <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card do colaborador</h2>
-                <CampoTexto label="Nome" placeholder="Digite seu nome" />
-                <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
+                <CampoTexto obrigatorio={true} label="Nome" placeholder="Digite seu nome" />
+                <CampoTexto obrigatorio={true} label="Cargo" placeholder="Digite seu cargo" />
                 <CampoTexto label="Imagem" placeholder="Digite o endereço da imagem" />
+                <ListaSuspensa obrigatorio={true} label="Time" itens={times}/>
+                <Botao>Criar Card</Botao>
             </form>
         </section>
     )
