124cb799badb23a086b0b642001b57ac5e2866d0
Nathanbsy <nathanbs1227@gmail.com>
2024-09-28 11:17:01 -0300

    Criando banner e formulario em React

---
+++ src/componentes/banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css"
+
+//Criando o banner
+export const Banner = () => {
+    //JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal do Organo"/>
+        </header>
+        
+    )
+}
